44641aa7ad6fc1ca9855fccecaccbbb3f23e2d9b
Jdavidrb <jdavidruanob@gmail.com>
2025-02-11 23:57:53 -0500

    Avances en el navBar y heroPage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,21 @@ import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">Sura Plans</div>
-      <ul className="navbar-links">
-        <li><a href="#features">Features</a></li>
-        <li><a href="#plans">Plans</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+    <nav className='navbar'>
+      <div className='navbar-content'>
+        <div className='navbar-left'>
+          <a href='#' className='navbar-link'>
+            Ascesoria
+            <img src='/src/assets/wpp-icon.png' alt='Ascesoria' className='navbar-icon' />
+          </a>
+        </div>
+        <div className='navbar-center'>
+          <img src='/src/assets/seguros-sura-nav.png' alt='Logo' className='navbar-logo' />
+        </div>
+        <div className='navbar-right'>
+          <a href='#' className='navbar-link'>Ver Planes</a>
+        </div>
+      </div>
     </nav>
   );
 };
