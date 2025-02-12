a1d1ec07b0c25d51503f2b85fe137a9ba4730f18
Jdavidrb <jdavidruanob@gmail.com>
2025-02-12 18:08:29 -0500

    Mejoras en el css de navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
-  return (
-    <nav className='navbar'>
-      <div className='navbar-content'>
-        <div className='navbar-left'>
-          <a href='#' className='navbar-link'>
-            Ascesoria
-            <img src='/src/assets/wpp-icon.png' alt='Ascesoria' className='navbar-icon' />
-          </a>
+    return (
+        <div className='container'>
+          <nav className='navbar'>
+          <div className='navbar-left'>
+              <a href='#' className='navbar-link'>
+              Ascesoria
+              <img src='/src/assets/wpp-icon.png' alt='Ascesoria' className='navbar-icon' />
+              </a>
+          </div>
+          <div className='navbar-center'>
+              <img src='/src/assets/seguros-sura-nav.png' alt='Logo' className='navbar-logo' />
+          </div>
+          <div className='navbar-right'>
+              <a href='#' className='navbar-link'>Ver Planes</a>
+          </div>
+          </nav>
         </div>
-        <div className='navbar-center'>
-          <img src='/src/assets/seguros-sura-nav.png' alt='Logo' className='navbar-logo' />
-        </div>
-        <div className='navbar-right'>
-          <a href='#' className='navbar-link'>Ver Planes</a>
-        </div>
-      </div>
-    </nav>
-  );
+    );
 };
 
 export default Navbar;
