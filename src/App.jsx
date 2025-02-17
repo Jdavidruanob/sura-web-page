2ea4f64ea0c210867e4ff3c83682ecc9db96b55b
Jdavidrb <jdavidruanob@gmail.com>
2025-02-17 17:34:24 -0500

    Listo para merge in develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import './styles/App.css'
-import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
-import PlansSection from './components/PlansSection'
+
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <HeroSection />
-      <PlansSection/>
-      
-      
       
     </div>
   )
