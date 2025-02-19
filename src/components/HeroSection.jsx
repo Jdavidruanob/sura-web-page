2b930c9c689135ca6c61f658d324c588b828adef
Jdavidrb <u.jdavidrb@gmail.com>
2025-02-19 00:56:30 -0500

    Hero seccion y navBar casi terminados

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,40 @@
+// HeroSection.jsx
 import React from 'react';
 import '../styles/HeroSection.css';
-import heroImage from '../assets/hero-image.png'; // Adjust the path as necessary
+import heroImage from '../assets/hero-image.png';
 
 const HeroSection = () => {
+  const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      const offset = 100;
+      const elementPosition = element.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - offset;
+      
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+      });
+    }
+  };
+
   return (
     <section className="hero-section">
       <div className="text-container">
         <h1 className='title-hero'>Protege tu salud y la de tu familia con Sura</h1>
-        <p className='subtitle-hero'>Medicina prepagada de la mas alta calidad</p>
-        <button id='button-hero'>Dejanos guiarte</button>
+        <p className='subtitle-hero'>Descubre tu plan ideal de medicina prepagada.</p>
+        
+        <div className="social-proof">
+          <p className="proof-text">+20 de las mejores clínicas de Colombia</p>
+          <p className="proof-text">+420,000 familias ya eligieron a Sura</p>
+        </div>
+
+        <button 
+          id='button-hero'
+          onClick={() => scrollToSection('servicios')}
+        >
+          Dejanos guiarte
+        </button>
       </div>
       <div className="image-container">
         <img src={heroImage} alt="Health and Family" />
