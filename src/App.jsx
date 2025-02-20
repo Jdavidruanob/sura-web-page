92b474ec036ac2fdb33d4089c63733feedc73375
ViejoPipe <andresacosta302005@gmail.com>
2025-02-20 16:02:53 -0500

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import CommentSection from './components/CommentSection'
 import TablePlans from './components/tablePlans'
 import PlansSection from './components/PlansSection'
 import TablePriceSection from './components/TablePriceSection'
+import Footer from './components/Footer'
+
 function App() {
   return ( 
     <div>
@@ -18,6 +20,8 @@ function App() {
       <TablePriceSection/>
       <TablePlans/>
       <CommentSection/>
+      <Footer/>
+
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="left-column">
+        <p><strong>Soporte Comercial:</strong></p>
+        <p>Ariel Palomo Chavarro</p>
+        <p>Phone: 18001024376 / +91 11 4057 8489</p>
+        <p>Email: <strong>support@heroibil.com</strong></p>
+      </div>
+      <div className="vertical-line"></div>
+      <div className="right-column">
+        <p>Ariel Palomo Chavarro.</p>
+        <p>Ariel como esta.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
