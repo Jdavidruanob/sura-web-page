f7f9f2f42a657635e52ea6b30d7bb38e2f40c1f4
ViejoPipe <andresacosta302005@gmail.com>
2025-02-21 01:06:30 -0500

    Pagina pulida

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/Footer.css';
+import logo1 from '../assets/Logo-SURA-blanco 1.svg';
 
 const Footer = () => {
   return (
     <div className="footer">
+      <img src={logo1} alt="Logo SURA" className="footer-logo" />
       <div className="left-column">
         <p><strong>Soporte Comercial:</strong></p>
         <p>Ariel Palomo Chavarro</p>
