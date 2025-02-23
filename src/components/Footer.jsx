c2d7b7bc5c75c795330f7fc7c8533e37c3b6d6d8
Jdavidrb <u.jdavidrb@gmail.com>
2025-02-22 20:25:05 -0500

    merge to develop complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import '../styles/Footer.css';
-<<<<<<< HEAD
-=======
 import logo1 from '../assets/Logo-SURA-blanco 1.svg';
->>>>>>> develop
 
 const Footer = () => {
   return (
     <div className="footer">
-<<<<<<< HEAD
-=======
       <img src={logo1} alt="Logo SURA" className="footer-logo" />
->>>>>>> develop
       <div className="left-column">
         <p><strong>Soporte Comercial:</strong></p>
         <p>Ariel Palomo Chavarro</p>
