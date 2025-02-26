462489ec0e4fcdc0d9dc1a1f41d2f76b412a68cc
Jdavidrb <u.jdavidrb@gmail.com>
2025-02-25 20:35:42 -0500

    Agregar imagenes planes

--- src/components/PlansSection.jsx
+++ src/components/PlansSection.jsx
@@ -1,7 +1,8 @@
 import './../styles/PlansSection.css';
 import PlanCard from '../components/PlanCard';
 import evoluciona_plan_img from '../assets/evoluciona-plan-img.png';
-
+import global_img from '../assets/global.png'
+import clasico_img from '../assets/clasico.png'
 const PlansSection = () => {
   return (
     <div id="plans-section">
@@ -10,21 +11,21 @@ const PlansSection = () => {
         <div id="plans-cards-container">
             <PlanCard
                 title="Plan evoluciona"
-                image={evoluciona_plan_img}
+                image={global_img}
                 paragraph="Podrás encontrar en un plan económico coberturas en temas de hospitalización, cirugías, consultas con las especialidades, exámenes de laboratoria y convenios con las mas importantes clínicas del país"
                 borderColor="#e2e72b" // Color azul claro para el borde superior
             />
                 <PlanCard
                 title="Plan clasico"
-                image={evoluciona_plan_img}
+                image={clasico_img}
                 paragraph="Este programa te permite tener un directorio medico mas amplio coberturas de emergencias fuera del país, rembolsos y cobertura ilimitada en Colombia"
-                borderColor="#2e6df5" // Otro tono de azul para el borde superior
+                borderColor="#04a7cc" // Otro tono de azul para el borde superior
             />
                 <PlanCard
                 title="Plan global"
-                image={evoluciona_plan_img}
+                image={global_img}
                 paragraph="El plan global nos gozar de una cobertura mas amplianos dentro y fuera del pais, convenio con las Clínicas importantes, toda la atencion en hospitalizacion, cirigias, gastos hospitalarios."
-                borderColor="#04a7cc" // Otro tono de azul para el borde superior
+                borderColor="#2a61ff" // Otro tono de azul para el borde superior
             />
         </div>
         
